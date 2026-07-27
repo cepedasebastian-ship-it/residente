3d19a0d7fdb290da8a84d4aa1fa6f9dc623e63cd
entropiastudios <sebastian@entropia-studios.com>
2026-07-26 22:17:33 -0300

    Widen layout to use more of large screens, side-by-side trip card layout, fix site URL and add OG image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://experiencia-residente.netlify.app',
+  site: 'https://experiencia-residente.pages.dev',
 });
